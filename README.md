5964c114b4675e387dc4c9d176327ac32819750a
Yuri <yuri.karadzhov@gmail.com>
2020-03-16 00:02:16 +0200

    Fix import before export

--- README.md
+++ README.md
@@ -9,6 +9,9 @@ npm install hqjs@babel-plugin-transform-named-export-to-destructure
 # Transformation
 Plugin makes destructure export work with modules that only provide default export.
 ```js
+import css from '@emotion/css';
+console.log(css);
+export { default as css } from '@emotion/css';
 import {x as y, h as l} from 'z';
 console.log(y);
 export {x as y} from 'z';
@@ -20,14 +23,16 @@ export * as w from 'zzz';
 
 will turn into
 ```js
-import { x as y, h as l } from 'z';
+import { css as _ref } from '@emotion/css';
+export const css = _ref;
+console.log(css);
+import { y as _ref2, h as l } from 'z';
+export const y = _ref2;
 console.log(y);
-import 'z';
-export const y = _ref;
-import { a as _ref2 } from 'zz';
-export const a = _ref2;
+import { a as _ref3 } from 'zz';
+export const a = _ref3;
 console.log(a);
 import { b } from 'zz';
-import * as _ref3 from 'zzz';
-export const w = _ref3;
+import * as _ref4 from 'zzz';
+export const w = _ref4;
 ```
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const importVisitor = (t, add, src, name, start) => ({
+const importVisitor = (t, add, {src, name, start, exportDecl, importName}) => ({
   ImportDeclaration(nodePath) {
     const { specifiers, source } = nodePath.node;
     if (source.value != src) return;
@@ -6,7 +6,12 @@ const importVisitor = (t, add, src, name, start) => ({
       const specs = specifiers.filter(spec => name != spec.local.name);
       if (specs.length > 0) nodePath.node.specifiers = specs;
       else nodePath.remove();
-    } else add.value = false;
+    } else {
+      const specs = specifiers.map(spec => spec.local.name === name ? t.importSpecifier(importName, spec.local) : spec);
+      nodePath.node.specifiers = specs;
+      nodePath.insertAfter(exportDecl)
+      add.value = false;
+    }
   }
 });
 
@@ -22,20 +27,28 @@ module.exports = function ({ types: t }) {
           const importSpecifier = specifier.local ?
             t.importSpecifier(importName, specifier.local) :
             t.importNamespaceSpecifier(importName);
-          const add = {value: true};
-          const program = nodePath.findParent(p => p.isProgram());
-          program.traverse(importVisitor(t, add, source.value, specifier.exported.name, nodePath.node.start));
-          if (add.value) importSpecifiers.push(importSpecifier);
           const exportDecl = t.exportNamedDeclaration(
             t.variableDeclaration('const', [
               t.variableDeclarator(specifier.exported, importName)
             ]),
             []
           );
-          nodePath.insertAfter(exportDecl);
+          const add = {value: true};
+          const program = nodePath.findParent(p => p.isProgram());
+          program.traverse(importVisitor(t, add, {
+            src: source.value,
+            name: specifier.exported.name,
+            start: nodePath.node.start,
+            exportDecl,
+            importName,
+          }));
+          if (add.value) {
+            importSpecifiers.push(importSpecifier);
+            nodePath.insertAfter(exportDecl);
+          }
         }
-        const importDecl = t.importDeclaration(importSpecifiers, source);
-        nodePath.replaceWith(importDecl);
+        if (importSpecifiers.length > 0) nodePath.replaceWith(t.importDeclaration(importSpecifiers, source));
+        else nodePath.remove();
       }
     }
   };
