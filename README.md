44d350562d0bb49581b35ee22a94cbfed6a7cd69
Yuri <yuri.karadzhov@gmail.com>
2020-03-15 23:22:40 +0200

    Fix import-export pairs

--- README.md
+++ README.md
@@ -9,19 +9,25 @@ npm install hqjs@babel-plugin-transform-named-export-to-destructure
 # Transformation
 Plugin makes destructure export work with modules that only provide default export.
 ```js
+import {x as y, h as l} from 'z';
+console.log(y);
 export {x as y} from 'z';
 export {a} from 'zz';
+console.log(a);
+import {a, b} from 'zz';
 export * as w from 'zzz';
-export * from 'zzzz';
 ```
 
 will turn into
 ```js
-import { x as _ref } from 'z';
+import { x as y, h as l } from 'z';
+console.log(y);
+import 'z';
 export const y = _ref;
 import { a as _ref2 } from 'zz';
 export const a = _ref2;
+console.log(a);
+import { b } from 'zz';
 import * as _ref3 from 'zzz';
 export const w = _ref3;
-export * from 'zzzz';
 ```
--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const importVisitor = (t, add, src, name, start) => ({
+  ImportDeclaration(nodePath) {
+    const { specifiers, source } = nodePath.node;
+    if (source.value != src) return;
+    if (nodePath.node.start > start) {
+      const specs = specifiers.filter(spec => name != spec.local.name);
+      if (specs.length > 0) nodePath.node.specifiers = specs;
+      else nodePath.remove();
+    } else add.value = false;
+  }
+});
+
 module.exports = function ({ types: t }) {
   return {
     visitor: {
@@ -8,19 +20,20 @@ module.exports = function ({ types: t }) {
         for (const specifier of specifiers) {
           const importName = nodePath.scope.generateUidIdentifierBasedOnNode('exportDestructure');
           const importSpecifier = specifier.local ?
-			t.importSpecifier(importName, specifier.local) :
-			t.importNamespaceSpecifier(importName);
-          importSpecifiers.push(importSpecifier);
+            t.importSpecifier(importName, specifier.local) :
+            t.importNamespaceSpecifier(importName);
+          const add = {value: true};
+          const program = nodePath.findParent(p => p.isProgram());
+          program.traverse(importVisitor(t, add, source.value, specifier.exported.name, nodePath.node.start));
+          if (add.value) importSpecifiers.push(importSpecifier);
           const exportDecl = t.exportNamedDeclaration(
             t.variableDeclaration('const', [
               t.variableDeclarator(specifier.exported, importName)
             ]),
             []
           );
-          console.log(exportDecl);
           nodePath.insertAfter(exportDecl);
         }
-        console.log(source, importSpecifiers);
         const importDecl = t.importDeclaration(importSpecifiers, source);
         nodePath.replaceWith(importDecl);
       }
